cd5ad2a1522312ede22a8031094ba7fb0696bac9
Zach Manno <zdm5045@psu.edu>
2017-04-09 00:12:46 -0400

    added blocks and started collision function

---
+++ Assets/Powerballscript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerballscript : MonoBehaviour {
+
+	public int totalpoints; 
+
+	// Use this for initialization
+	void Start () {
+		totalpoints = 0; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Check to see if hitting something 
+		// If hitting, get point value of object and add to your points. Destroy hit object. 
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		// Get points and add to total points.
+		// Destroy collided object. 
+
+		collision.collider.gameObject.name;
+
+		foreach (ContactPoint contact in collision.contacts)
+		{
+			contact.thisCollider.gameObjec
+		}
+
+		if (collision.relativeVelocity.magnitude > 2)
+			audio.Play();
+	}
+}
