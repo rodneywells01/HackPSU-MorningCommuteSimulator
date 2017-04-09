a93704feb7efecd9dc8c2136085e0da685472e51
Rodney Wells <Rodney Wells>
2017-04-09 03:11:15 -0400

    Valves library as well as all of my custom made assets and scripts.

---
+++ Assets/blockscript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //transform.position = GameObject.Find("Controller (left)").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/paddlescript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddlescript : MonoBehaviour {
+
+    // Use this for initialization
+    Rigidbody myrb;
+
+    void Start () {
+        myrb = GetComponent<Rigidbody>();
+        //CollisionDetectionMode.ContinuousDynamic;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+
+        if (collision.gameObject.name.Contains("Ball"))
+        {            
+            GameObject entityhit = collision.gameObject;
+            Rigidbody body = entityhit.GetComponent<Rigidbody>();
+            body.AddForce(myrb.velocity * -100);
+            Debug.Log("Hit!");
+            //body.AddForce()
+        }
+
+    }
+}
